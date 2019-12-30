0eabd39ca9beb65126059100c8f38106f05e7c68
Vicx95 <wiktor.zyla31@gmail.com>
2019-12-30 19:58:07 +0100

    add layouts: login, menu, addemployee, editemployee, schedule, reports, presence

--- regworktime.cpp
+++ regworktime.cpp
@@ -13,3 +13,35 @@ Regworktime::~Regworktime()
     delete ui;
 }
 
+
+void Regworktime::on_LogoutButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void Regworktime::on_backtoMenufromaddEmployee_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void Regworktime::on_backToMenuFromEditEmployee_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+
+void Regworktime::on_backToMenuFromSchedule_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void Regworktime::on_backToMenuFromPresence_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void Regworktime::on_backToMenuFromReport_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
